3f39629a98674823fda2c8fde7ff0e4f8406b35d
simalaoshi <1742495214@qq.com>
2022-10-25 14:29:15 +0800

    1，增加原表优化

--- ExportLua/xlrd_lua.py
+++ ExportLua/xlrd_lua.py
@@ -21,13 +21,14 @@ def parse_excel(file_path):
     workbook = xlrd.open_workbook(file_path)
     for i in range(workbook.nsheets):
         sheet = workbook.sheet_by_index(i)
-        fields, describes, field_types, export_filter = {}, {}, {}, {}
+        fields, describes, field_types, export_filter, default_val = {}, {}, {}, {}, {}
         for col in range(sheet.ncols):
             fields[col] = sheet.cell_value(0, col)
             describes[col] = sheet.cell_value(1, col)
             field_types[col] = sheet.cell_value(2, col)
             export_filter[col] = sheet.cell_value(4, col)
-
+            default_val[col] = sheet.cell_value(6, col)
+        print(default_val)
         sb = "local %s = { \n" % sheet.name
         for row in range(sheet.nrows - 6):
             row += 6
@@ -35,13 +36,25 @@ def parse_excel(file_path):
             for v in fields.items():
                 # 过滤不需要的列，比如描述，或者服务器列客户端列
                 if int(export_filter[v[0]]) != 0:
-                    s += "\t\t['%s'] = %s, \n" % (v[1], format_val(field_types[v[0]], sheet.cell_value(row, v[0])))
+                    if default_val[v[0]] != sheet.cell_value(row, v[0]):
+                        s += "\t\t['%s'] = %s, \n" % (v[1], format_val(field_types[v[0]], sheet.cell_value(row, v[0])))
             s += "\t},\n"
             sb += s
 
-            print(s)
+            # print(s)
         sb += "}\n"
 
+        # 写入原表优化
+
+        meta = "local default = { \n"
+        for v in fields.items():
+            meta += "\t['%s'] = %s, \n" % (v[1], format_val(field_types[v[0]], default_val[v[0]]))
+        meta += "}\n"
+        meta += "local mt = {\n\t__index = default,\n\t__newindex = function()\n \t--自己实现设置限制 \n\tend \n} \n"
+        meta += "local setmetatable = setmetatable\n"
+        meta += "local pairs = pairs\n"
+        meta += "for _, v in pairs(%s) do \n\tsetmetatable(v, mt) \nend\n" % sheet.name
+        sb += meta
         # 写入字段描述
         describe = ""
         for v in fields.items():
